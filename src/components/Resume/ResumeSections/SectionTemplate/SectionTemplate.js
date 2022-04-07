8e98d2249ccece23014e9fafb1d14d66440892af
Shubham Shinde <30952528+ShindeShubhamm@users.noreply.github.com>
2022-04-07 21:26:24 +0530

    Employment status changes

--- src/components/Resume/ResumeSections/SectionTemplate/SectionTemplate.js
+++ src/components/Resume/ResumeSections/SectionTemplate/SectionTemplate.js
@@ -1,45 +1,54 @@
-import React from 'react'
+import React from 'react';
 
-import "./SectionTemplate.scss"
+import './SectionTemplate.scss';
 
 const SectionTemplate = (props) => {
-  const { data } = props
+    const { data } = props;
 
-  return (
-    <div className="section">
-      <div className="section-header">
-        {data.icon}
-        <h2 className="section-title">{data.title}</h2>
-      </div>
-      <div className="section-content">
-        {data.values.map((v, index) => (
-          <div className="section-data" key={index}>
-            <div className="section-ui">
-              <div className="sui-circle"></div>
-              <div className="sui-line"></div>
+    return (
+        <div className='section'>
+            <div className='section-header'>
+                {data.icon}
+                <h2 className='section-title'>{data.title}</h2>
             </div>
-            <div className="section-text">
-              {v.title && <h2 className="sd-title">
-                {v.title}
-              </h2>}
-              {v.date && <p className="sd-date">
-                {v.date}
-              </p>}
-              {v.text && <p className="sd-text">
-                {v.text}
-              </p>}
-              {v.listData && <ul className="sd-list">
-                {v.listData.map((listItem, index) => (
-                  <li key={index} className="sd-text sd-listitem">{listItem}</li>
+            <div className='section-content'>
+                {data.values.map((v, index) => (
+                    <div className='section-data' key={index}>
+                        <div className='section-ui'>
+                            <div className='sui-circle'></div>
+                            <div className='sui-line'></div>
+                        </div>
+                        <div className='section-text'>
+                            {v.title && (
+                                <h2 className='sd-title'>
+                                    {v.title}{' '}
+                                    {v.type && (
+                                        <span className='subTitle'>
+                                            {v.type}
+                                        </span>
+                                    )}
+                                </h2>
+                            )}
+                            {v.date && <p className='sd-date'>{v.date}</p>}
+                            {v.text && <p className='sd-text'>{v.text}</p>}
+                            {v.listData && (
+                                <ul className='sd-list'>
+                                    {v.listData.map((listItem, index) => (
+                                        <li
+                                            key={index}
+                                            className='sd-text sd-listitem'
+                                        >
+                                            {listItem}
+                                        </li>
+                                    ))}
+                                </ul>
+                            )}
+                        </div>
+                    </div>
                 ))}
-              </ul>}
-
             </div>
-          </div>
-        ))}
-      </div>
-    </div>
-  )
-}
+        </div>
+    );
+};
 
-export default SectionTemplate
+export default SectionTemplate;
--- src/data/employment.js
+++ src/data/employment.js
@@ -1,15 +1,45 @@
-import { IoMdBriefcase } from "react-icons/io"
+import { IoMdBriefcase } from 'react-icons/io';
 
 export const employmentData = () => {
-  return {
-    title: "Employment History",
-    icon: <IoMdBriefcase className="section-icon" size={22} />,
-    values: [
-      {
-        title: "MERN Developer Intern at 10xGrowth",
-        date: "June 2020 - December 2020",
-        listData: ["Used technologies like NextJS, ReactJS, Firebase, MongoDB, Node-LoopbackJS and other libraries and utilities to solve complex problems.", "Created user friendly and attractive user interfaces using Material UI and SASS.", "Wrote business logic in LoopbackJS and exposed to endpoints.", "Formulated and implemented performance improvement techniques, minimizing web page load time.", "Wrote reusable and clean code."]
-      }
-    ]
-  }
-}
+    return {
+        title: 'Employment History',
+        icon: <IoMdBriefcase className='section-icon' size={22} />,
+        values: [
+            {
+                title: 'UX Engineer at Digité Inc.',
+                type: 'Full Time',
+                date: 'August 2021 - Present',
+                listData: [
+                    'Ideating over user experience and creating prototypes.',
+                    'Solve tough UX problems with creative and engaging solutions that make our product a delight to use.',
+                    'Designing product screens for the web-based platform using tools like Figma and Inkscape.',
+                    'Building web application as per designs using UI-Framework ReactJS, Redux and Material UI.',
+                    'Integrating business logic endpoints to bring various functionalities.',
+                ],
+            },
+            {
+                title: 'Frontend Developer at Digité Inc.',
+                type: 'Internship',
+                date: 'January 2021 - July 2021',
+                listData: [
+                    'Designed widgets that can be used across multiple products and cross-platform.',
+                    'Built web components and logic utilities using Javascript, JSX and SASS.',
+                    'Learned to work in an agile environment with daily stand-ups.',
+                    'Wrote unit tests in Jest and Enzyme to ensure code is 100% tested and bug-free.',
+                ],
+            },
+            {
+                title: 'MERN Developer at 10xGrowth',
+                type: 'Internship',
+                date: 'June 2020 - December 2020',
+                listData: [
+                    'Used technologies like NextJS, ReactJS, Firebase, MongoDB, Node-LoopbackJS and other libraries and utilities to solve complex problems.',
+                    'Created user friendly and attractive user interfaces using Material UI and SASS.',
+                    'Wrote business logic in LoopbackJS and exposed to endpoints.',
+                    'Formulated and implemented performance improvement techniques, minimizing web page load time.',
+                    'Wrote reusable and clean code.',
+                ],
+            },
+        ],
+    };
+};
